ff1ff8f0062977b2fffc06906ceefd9a529b3255
usrbinkat <usrbinkat@braincraft.io>
2021-07-20 21:56:21 -0700

    add eks cluster
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,9 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
+import * as eks from "@pulumi/eks";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+// Create an EKS cluster with the default configuration.
+const cluster = new eks.Cluster("keks");
+
+// Export the cluster's kubeconfig.
+export const kubeconfig = cluster.kubeconfig;
