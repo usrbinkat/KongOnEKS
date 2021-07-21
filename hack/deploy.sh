92bd8db200f9ade056a03e07edc03a8f89fe0834
usrbinkat <usrbinkat@braincraft.io>
2021-07-20 21:54:12 -0700

    cleanup
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -1,19 +1,7 @@
 #!/bin/bash -x
-git_repo=KongOnEKS
 project="deploy-${git_repo}"
-tmpDir="${HOME}/Git/tmp/pulumi-runner"
-
-rm -fr ${tmpDir}
-mkdir -p ${tmpDir}/{ssh,aws,kube,gitconfig}
-cp -fr ${HOME}/.gitconfig ${tmpDir}/gitconfig
-cp -fr ${HOME}/.kube ${tmpDir}/kube
-cp -fr ${HOME}/.aws ${tmpDir}/aws
-cp -fr ${HOME}/.ssh ${tmpDir}/ssh
-
-docker run -it --rm --pull always \
+time docker run -it --rm --pull always \
     -v ${PWD}:/pulumi:z \
-    --env-file /tmp/env \
-    --entrypoint pulumi \
-    --name "${project}" -h "${project}" --user root \
+    --env-file $HOME/Git/tmp/pulumi-runner/env \
    ghcr.io/usrbinkat/pulumi-runner \
-     up --stack KongOnEKS --yes --cwd /pulumi/pulumi --non-interactive
+     pulumi up --stack KongOnEKS --yes --cwd /pulumi/pulumi --non-interactive
