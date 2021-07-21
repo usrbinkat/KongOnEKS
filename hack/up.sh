3cffbcfc9f327c6b7ef6a01d13da5ddfcaa457fd
usrbinkat <usrbinkat@braincraft.io>
2021-07-21 13:37:24 -0700

    re-test deploy
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- hack/up.sh
+++ hack/up.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
-project="deploy-${git_repo}"
+project="pulumi-runner"
+
 time docker run -it --rm --pull always \
     -v ${PWD}:/pulumi:z \
-    --env-file $HOME/Git/tmp/pulumi-runner/env \
+    -e AWS_ACCESS_KEY_ID=$(awk '/aws_access_key_id/{print $3}' ~/.aws/credentials) \
+    -e AWS_SECRET_ACCESS_KEY=$(awk '/aws_secret_access_key/{print $3}' ~/.aws/credentials) \
+    -e PULUMI_ACCESS_TOKEN=$(awk -F'[",: ]' '/            "accessToken/{print $18}' ~/.pulumi/credentials.json) \
    ghcr.io/usrbinkat/pulumi-runner \
      pulumi up --stack KongOnEKS --yes --cwd /pulumi/pulumi --non-interactive
