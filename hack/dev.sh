8ef141fbb9a2b100fbb2de90199636054bab415b
usrbinkat <usrbinkat@braincraft.io>
2021-07-28 16:10:58 -0700

    add kong helm chart repo to runner image
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- hack/dev.sh
+++ hack/dev.sh
@@ -6,5 +6,5 @@ time docker run -it --rm --pull always \
     -e AWS_ACCESS_KEY_ID=$(awk '/aws_access_key_id/{print $3}' ~/.aws/credentials) \
     -e AWS_SECRET_ACCESS_KEY=$(awk '/aws_secret_access_key/{print $3}' ~/.aws/credentials) \
     -e PULUMI_ACCESS_TOKEN=$(awk -F'[",: ]' '/            "accessToken/{print $18}' ~/.pulumi/credentials.json) \
-    --entrypoint bash \
+    --entrypoint bash --workdir /pulumi/pulumi \
    ghcr.io/usrbinkat/pulumi-runner
