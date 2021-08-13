f9d306ddf8d4027c39f23f9baf5014ee4fb52c51
usrbinkat <usrbinkat@braincraft.io>
2021-08-13 08:00:56 -0700

    WIP debug urn duplication issue
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# KongOnEKS
+# KongOnEKS | Status: *rough draft*
 Pulumi IaC for EKS
+
+## Current Steps:
+```sh
+# Clone Repo
+git clone https://github.com/usrbinkat/KongOnEKS ~/KongOnEKS && cd ~/KongOnEKS
+
+# Run IaC Runner Container (local)
+./hack/dev.sh
+
+# Install NPM Packages
+npm install
+
+# Create Stack
+pulumi stack init usrbinkat/Gateway/KongHybridGatewayOnEKS
+pulumi config set --secret kong:dbPassword password
+pulumi config set aws:region us-east-1
+pulumi config set eksCluster:clusterName KongOnEKS
+pulumi up
+```
+
+# Workarounds to be resolved later
+```sh
+kubectl create secret generic kong-enterprise-license -n kong --from-file=license=${HOME}/.kong-license-data/license.json
+podman run -it --rm --pull always --user root -v /tmp/kong:/tmp/kong:z docker.io/kong/kong -- kong hybrid gen_cert /tmp/kong/tls.crt /tmp/kong/tls.key
+kubectl create secret tls kong-cluster-cert --namespace kong --cert=/tmp/kong/tls.crt --key=/tmp/kong/tls.key
+kubectl create secret generic kong-enterprise-superuser-password -n kong --from-literal=password='password'
+```
--- pulumi/index.ts
+++ pulumi/index.ts
@@ -4,26 +4,51 @@ import * as k8s    from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
 import * as awsx   from "@pulumi/awsx";
 
-// Set Database Password:
-// ~$ pulumi config set --stack KongOnEKS --secret kong:dbPassword password
-const config = new pulumi.Config("kong");
-const dbPassword = config.requireSecret("dbPassword");
+
+////////////////////////////////////////////////////////////////////////////////
+// Set Variables:
+//   ~$ pulumi config set --stack KongOnEKS --secret kong:dbPassword password
+const kongConfig = new pulumi.Config("kong")
+const dbPassword = kongConfig.requireSecret("dbPassword")
+const eksConfig = new pulumi.Config("eksCluster")
+const clusterName = eksConfig.require("clusterName")
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Create a VPC
-// TODO:
-// - variablize cluster name
-// - implement tagging on all resources
-const vpc = new awsx.ec2.Vpc("keks-vpc", {
+// TODO: variablize cluster name
+// TODO: implement tagging on all resources
+const vpc = new awsx.ec2.Vpc(`${clusterName}-vpc`, {
   cidrBlock: "172.16.0.0/16",
   numberOfNatGateways: 1,
   numberOfAvailabilityZones: 3,
+  tags: {
+    Name: `${clusterName}`
+  },
   subnets: [
     { type: "public",  name: "public"  }, 
-  //TODO: enable private subnets
+  //TODO: enable vpc private subnets
   //  { type: "private", name: "private" }
   ]
-},{customTimeouts: {create: "30m"}});
+},{customTimeouts: {create: "30m"}})
+export const vpcId = vpc.id;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Create RDS Kong Configuration Data Store
+//
+// Terraform for review & enhancement considerations
+// - https://github.com/hashicorp/terraform-provider-aws/tree/master/examples/rds
+
+// TODO: variablize instance type
+// TODO: variablize database name
+// TODO: variablize database uname
+// TODO: disable publicly accessible
+// TODO: variablize allocated storage
+// TODO: convert rds.Instance to rds.Cluster
+// REF: https://www.pulumi.com/docs/reference/pkg/aws/rds/instance/
+// REF: https://www.pulumi.com/docs/guides/crosswalk/aws/vpc/#configuring-subnets-for-a-vpc
+// REF: https://github.com/pulumi/pulumi-aws/blob/0b73446ce5361fcd9f313d7e5ac36e5668936d31/sdk/nodejs/rds/cluster.ts#L75
 
 // RDS Security Group
 const rdsSecurityGroup = new aws.ec2.SecurityGroup("dbsecgrp", {
@@ -48,33 +73,21 @@ const rdsSecurityGroup = new aws.ec2.SecurityGroup("dbsecgrp", {
 });
 
 // RDS Subnet attachment
-// TODO: convert to rds.Cluster
-// REF: https://www.pulumi.com/docs/guides/crosswalk/aws/vpc/#configuring-subnets-for-a-vpc
-// REF: https://github.com/pulumi/pulumi-aws/blob/0b73446ce5361fcd9f313d7e5ac36e5668936d31/sdk/nodejs/rds/cluster.ts#L75
 const dbSubnets = new aws.rds.SubnetGroup("dbsubnets", {
   subnetIds: vpc.publicSubnetIds,
-  // TODO migrate to private subnet id(s)
+  // TODO migrate rds to private subnet id(s)
   //subnetIds: vpc.privateSubnetIds,
+  tags: {
+    Name: `${clusterName}-rdsPostgresSubnetGroup`
+  },
 });
 
 // RDS Postgres Database
-// Set RDS Password:
-//   ~$ pulumi --stack KongOnEKS config set dbPassword
-// - kong configuration store
-// https://www.pulumi.com/docs/reference/pkg/aws/rds/instance/
-// Terraform for review & enhancement considerations
-// - https://github.com/hashicorp/terraform-provider-aws/tree/master/examples/rds
 const db = new aws.rds.Instance("postgresdb", {
-  // TODO: 
-  // - variablize database name
-  // - variablize database uname
-  // - variablize instance type
-  // - variablize allocated storage
   name: "kong",
   username: "kong",
   password: dbPassword,
-  instanceClass: "db.t2.micro",
-  // TODO: disable publicly accessible
+  instanceClass: "db.t3.micro",
   publiclyAccessible: true,
   allocatedStorage: 20,
   engine: "postgres",
@@ -85,19 +98,20 @@ const db = new aws.rds.Instance("postgresdb", {
   dbSubnetGroupName: dbSubnets.id,
   skipFinalSnapshot: true,
   tags: {
-    Name: "KongOnEKS_PostgresDB"
+    Name: `${clusterName}-rdsPostgresDB`
   }
-});
+},{customTimeouts: {create: "30m"}});
 export const rdsIp = db.address;
 
-// Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("keks-cluster", {
+////////////////////////////////////////////////////////////////////////////////
+// EKS cluster
+const cluster = new eks.Cluster(`${clusterName}-cluster`, {
   vpcId: vpc.id,
   minSize: 2,
   maxSize: 6,
   desiredCapacity: 3,
   publicSubnetIds: vpc.publicSubnetIds,
-  //TODO: enable private subnets
+  //TODO: convert eks to private subnets
   //privateSubnetIds: vpc.privateSubnetIds,
   nodeAssociatePublicIpAddress: true,
   enabledClusterLogTypes: [
@@ -105,10 +119,13 @@ const cluster = new eks.Cluster("keks-cluster", {
     "audit",
     "authenticator",
   ],
-},{customTimeouts: {create: "30m"}});
+},{customTimeouts: {create: "120m"}});
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig;
+
+// Configure EKS KUBECONFIG provider
+export const provider = new k8s.Provider("k8s", {kubeconfig: kubeconfig})
 
 // Create S3 Bucket with KUBECONFIG as object
 // Pull kubeconfig from s3 via aws cli:
@@ -119,24 +136,18 @@ const keksAdminBucketObject = cluster.kubeconfig.apply(
   new aws.s3.BucketObject("keksAdminBucketObject", {
     key: "kubeconfig",
     bucket: keksAdminBucket.id,
-    source: new pulumi.asset.StringAsset(JSON.stringify(config)),
-    /*
-    TODO: test following variation due to advice "Creating resources in apply is discoraged"
-    source: cluster.kubeconfig.apply(s => new pulumi.asset.StringAsset(JSON.stringify(s))),
-    */
     serverSideEncryption: "aws:kms",
+    source: cluster.kubeconfig.apply(s => new pulumi.asset.StringAsset(JSON.stringify(s))),
 }))
+export const adminBucketName = keksAdminBucket.id;
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Kong HELM Chart Deploy
 
-// Configure EKS KUBECONFIG provider
-export const provider = new k8s.Provider("k8s", {kubeconfig: kubeconfig})
-
 // create namespace 'kong'
 const namespace = new k8s.core.v1.Namespace("ns", {metadata: {name: "kong",}},{provider: provider});
 
-/*
 // Deploy Kong Enterprise Controlplane from kong/kong helm chart.
 // TODO:
 //   - RFE: https://github.com/pulumi/pulumi-kubernetes/issues/555
@@ -145,56 +156,37 @@ const kongGatewayCP = new k8s.helm.v3.Chart("controlplane", {
   repo: "kong",
   chart: "kong",
   namespace: "kong",
-  fetchOpts:{
-    repo: "https://charts.konghq.com/",
-  },
+  fetchOpts: {repo: "https://charts.konghq.com/"},
   values: {
+//  plugins: "{}",
+    replicaCount: "2",
+    secretVolumes: ["kong-cluster-cert"],
+    deployment: {kong: {enabled: true, daemonset: false}},
+    deploymentAnnotations: {"kuma.io/gateway": "enabled"},
+    proxy: {enabled: false},
     env: {
-      pg_port: "5432",
       role: "control_plane",
       prefix: "/kong_prefix/",
       database: "postgres",
-      // TODO: convert to `valueFrom Secret`
-      password: "password",
-      // TODO: variablize from config
+      log_level: "debug",
+      smtp_mock: "on",
+      portal: "on",
+      vitals: "on",
+      anonymous_reports: "off",
+      nginx_worker_processes: "2",
+      // TODO: variablize postgres user/db/port from config
       pg_user: "kong",
-      pg_password: dbPassword,
       pg_database: "kong",
-      // TODO: variablize postgres host from pulumi rds endpoint output
-      pg_host: "postgresdb2876999.cnc7tkeqsmj9.us-east-1.rds.amazonaws.com",
-      couster_cert: "/etc/secrets/kong-cluster-cert/tls.crt",
-      couster_cert_key: "/etc/secrets/kong-cluster-cert/tls.key"
-    },
-    enterprise: {
-      enabled: true,
-      // TODO: create secret from pulumi config https://git.io/JRBgk
-      license_secret: "kong-enterprise-license",
-      vitals: {
-        enabled: true
-      },
-    },
-    rbac: {
-      enabled: true
-    },
-    admin: {
-      enabled: true,
-      http: {
-        enabled: true,
-        servicePort: 8001,
-        containerPort: 8001
-      }
-    },
-    manager: {
-      enabled: true
-    },
-    portal: {
-      enabled: true
-    },
-    portalapi: {
-      enabled: true
-    },
-    proxy: {
-      enabled: false
+      pg_host: rdsIp, 
+      pg_port: "5432",
+      pg_password: dbPassword,
+      // TODO: convert super admin password to `valueFrom Secret`
+      password: "password",
+      cluster_cert: "/etc/secrets/kong-cluster-cert/tls.crt",
+      cluster_cert_key: "/etc/secrets/kong-cluster-cert/tls.key",
+      portal_cors_origins: "*",
+      portal_auth: "basic-auth",
+      audit_log: "on"
     },
     cluster: {
       enabled: true,
@@ -202,29 +194,193 @@ const kongGatewayCP = new k8s.helm.v3.Chart("controlplane", {
         enabled: true,
         servicePort: 8005,
         containerPort: 8005,
-      }
+        parameters: []
+      },
+      type: "ClusterIP"
     },
-    secretVolumes: ["kong-cluster-cert"],
+    status: {
+      enabled: true,
+      http: {
+        enabled: true,
+        servicePort: 8100,
+        containerPort: 8100,
+        parameters: []
+      },
+      tls: {enabled: false},
+    },
     ingressController: {
-      enabled: false,
-      installCRDs: false
+      enabled: true,
+      args: ["--v=5"],
+      installCRDs: false,
+      env: {
+        publish_service: "kong/controlplane-kong-proxy",
+        kong_admin_tls_skip_verify: true,
+        kong_admin_token: {
+          valueFrom: {
+            secretKeyRef: {
+              name: "kong-enterprise-superuser-password",
+              key: "password"
+            }
+          }
+        }
+      },
+      admissionWebhook: {
+        enabled: true,
+        failurePolicy: "Fail",
+        port: 8080
+      },
+      ingressClass: "kong",
+      rbac: {create: true},
+      serviceAccount: {create: true}
+      /*
+      // TODO: assign resource requests and limits
+      resources: {
+        limits: {cpu: "100m",memory: "256mi",},
+        requests: {cpu: "50m",memory: "128mi",}
+      }
+      */
     },
-    postgresql: {
-      enabled: false
+    enterprise: {
+      enabled: true,
+      rbac: {
+        enabled: true,
+        admin_gui_auth: "basic-auth",
+        session_conf_secret: "kong-session-config",
+        admin_gui_auth_conf_secret: "kong-admin-gui-auth-conf-secret"
+      },
+      license_secret: "kong-enterprise-license",
+      vitals: {enabled: true},
+      portal: {enabled: true},
+      smtp: {enabled: true}
     }
   },
 },{
   providers: {kubernetes: provider},
-  customTimeouts: {create: "30m"}
+  customTimeouts: {create: "5m"}
 });
+/*
+    manager: {
+      enabled: true,
+      type: "ClusterIP",
+      annotations: {
+        "konghq.com/protocol": "https"
+      },
+      http: {enabled: false},
+      tls: {
+        enabled: true,
+        servicePort: 8445,
+        containerPort: 8445,
+        parameters: ["http2"]
+      },
+      ingress: {
+        enabled: true,
+        annotations: {
+          "kubernetes.io/ingress.class": "kong"
+        },
+        tls: "manager.kong.kongoneks.lab",
+        hostname: "manager.kong.kongoneks.lab",
+        path: "/"
+      },
+      labels: "{}"
+    },
+    portal: {
+      enabled: true,
+      type: "ClusterIP",
+      annotations: {
+        "konghq.com/protocol": "https"
+      },
+      http: {
+        enabled: true,
+        servicePort: 8003,
+        containerPort: 8003,
+        parameters: []
+      },
+      tls: {
+        enabled: true,
+        servicePort: 8446,
+        containerPort: 8446,
+        parameters: ["http2"]
+      },
+      ingress: {
+        enabled: true,
+        annotations: {
+          "kubernetes.io/ingress.class": "kong"
+        },
+        tls: "portal.kong.kongoneks.lab",
+        hostname: "portal.kong.kongoneks.lab",
+        path: "/"
+      },
+      labels: "{}"
+    },
+    portalapi: {
+      enabled: true,
+      type: "ClusterIP",
+      annotations: {
+        "konghq.com/protocol": "https"
+      },
+      http: {
+        enabled: true,
+        servicePort: 8004,
+        containerPort: 8004,
+        parameters: []
+      },
+      tls: {
+        enabled: true,
+        servicePort: 8447,
+        containerPort: 8447,
+        parameters: ["http2"]
+      },
+      ingress: {
+        enabled: true,
+        annotations: {
+          "kubernetes.io/ingress.class": "kong"
+        },
+        tls: "papi.kong.kongoneks.lab",
+        hostname: "papi.kong.kongoneks.lab",
+        path: "/"
+      },
+      labels: "{}"
+    },
+    admin: {
+      enabled: true,
+      type: "ClusterIP",
+      annotations: {
+        "konghq.com/protocol": "https"
+      },
+      http: {enabled: false,},
+      tls: {
+        enabled: true,
+        servicePort: 8444,
+        containerPort: 8444,
+        parameters: ["http2"]
+      },
+      ingress: {
+        enabled: true,
+        annotations: {
+          "kubernetes.io/ingress.class": "kong"
+        },
+        tls: "manager.kong.kongoneks.lab",
+        hostname: "manager.kong.kongoneks.lab",
+        path: "/"
+      },
+      labels: "{}"
+    },
+    */
 
 // Deploy Kong Enterprise Controlplane from kong/kong helm chart.
-// TODO:
-//  - automate controlplane / dataplane pki trust secret
+// TODO: automate controlplane / dataplane pki trust secret
+// TODO: resolve Duplicate Resource URN
+/*
+  REF: https://www.pulumi.com/docs/intro/concepts/resources/#urns
+  Error:
+    Diagnostics:
+      kubernetes:apiextensions.k8s.io/v1:CustomResourceDefinition (kongplugins.configuration.konghq.com):
+        error: Duplicate resource URN 'urn:pulumi:KongHybridGatewayOnEKS::Gateway::kubernetes:helm.sh/v3:Chart$kubernetes:apiextensions.k8s.io/v1:CustomResourceDefinition::kongplugins.configuration.konghq.com'; try giving it a unique name
+*/
 const kongGatewayDP = new k8s.helm.v3.Chart("dataplane", {
   repo: "kong",
   chart: "kong",
-  namespace: "kong-proxy",
+  namespace: "kong",
   fetchOpts:{
     repo: "https://charts.konghq.com/",
   },
@@ -233,49 +389,31 @@ const kongGatewayDP = new k8s.helm.v3.Chart("dataplane", {
       database: "off",
       role: "data_plane",
       prefix: "/kong_prefix/",
-      cluster_control_plane: "gateway-kong-cluster.kong.svc.cluster.local:8005",
-      lua_ssl_trusted_certificate: "/etc/secrets/kong-cluster-cert/tls.crt",
       couster_cert: "/etc/secrets/kong-cluster-cert/tls.crt",
-      couster_cert_key: "/etc/secrets/kong-cluster-cert/tls.key"
+      couster_cert_key: "/etc/secrets/kong-cluster-cert/tls.key",
+      lua_ssl_trusted_certificate: "/etc/secrets/kong-cluster-cert/tls.crt",
+      cluster_control_plane: "controlplane-kong-cluster.kong.svc.cluster.local:8005"
     },
+    proxy: {enabled: true},
     admin: {enabled: false},
+    portal: {enabled: false},
+    cluster: {enabled: false},
+    manager: {enabled: false},
+    portalapi: {enabled: false},
     secretVolumes: ["kong-cluster-cert"],
-    ingressController: {
-      enabled: false,
-      installCRDs: false
-    },
-    manager: {
-      enabled: false
-    },
-    portal: {
-      enabled: false
-    },
-    portalapi: {
-      enabled: false
-    },
-    proxy: {
-      enabled: true
-    },
-    cluster: {
-      enabled: false
-    }
+    ingressController: {enabled: false, installCRDs: false}
   },
 },{
   providers: {kubernetes: provider},
   customTimeouts: {create: "30m"}
 });
 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Export Values
 // TODO:
 // - deprecate kubeconfig reliance
 // - leverage oidc / rbac natively on AWS for api auth
-
-// VPC ID
-export const vpcId = vpc.id;
-
-// name of admin kubeconfig bucket
-export const adminBucketName = keksAdminBucket.id;
 
 ////////////////////////////////////////////////////////////////////////////////
 /*
